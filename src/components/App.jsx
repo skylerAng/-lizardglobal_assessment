07acf8577f619185d2b404fc4893d8f8b943a908
Skyler Ang <skylerang96@yahoo.com>
2023-04-26 01:41:15 +0800

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,52 @@
-function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const App = () => {
+  const [data, setData]=useState([]);
+  debugger;
+  const getData = () => {
+      //const response = await fetch (`/api/posts`);
+      //const data = await response.json();
+      fetch('/api/posts')
+      .then((response) => response.json())
+      .then((jsonData) => {
+        console.log(jsonData);
+        debugger;
+        setData(jsonData);
+        debugger;
+        // setPosts(json.posts.slice(0, next));
+        // setOriginalPosts(json.posts);
+      })
+      .catch((err) => console.log(err));
+
+  }
+  useEffect(() => {
+    getData();
+   
+  }, []);
+
+  return <div>{
+    /* Complete the exercise here. */
+    <div className="App">
+    {
+        data ?.length > 0
+        ? (
+            <div className="container">
+                {data.map((item)=>(
+                
+                    <p>{item.about}</p>
+                ))}
+            </div>
+        ) : 
+        (
+            <div className='empty'>
+                <h2>No data found</h2>
+            </div>
+        )
+    }
+    </div>
+  
+  }</div>;
 }
 
 export default App;
